760e9e64da6af1772748bbaf344ba4bb963fc2df
Marc G Tan <mtan@e1z3r4p23.42.us.org>
2018-02-21 22:13:18 -0800

    lst add 2

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/21 22:12:51 by mtan              #+#    #+#             */
+/*   Updated: 2018/02/21 22:12:52 by mtan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*temp;
 
-	if(!(temp = (t_list *)malloc(sizeof(t_list) * content_size)))
+	if (!(temp = (t_list *)malloc(sizeof(t_list) * content_size)))
 		return (NULL);
 	if (!content)
 	{
@@ -17,30 +29,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 			return (NULL);
 		ft_memcpy(temp->content, content, content_size);
 		temp->content_size = content_size;
-
 	}
 	temp->next = NULL;
 	return (temp);
 }
-
-// t_list		*ft_lstnew(void const *content, size_t content_size)
-// {
-// 	t_list	*new;
-//
-// 	if (!(new = (t_list *)malloc(sizeof(t_list) * content_size)))
-// 		return (NULL);
-// 	if (!content)
-// 	{
-// 		new->content = NULL;
-// 		new->content_size = 0;
-// 	}
-// 	else
-// 	{
-// 		if (!(new->content = (void *)malloc(content_size)))
-// 			return (NULL);
-// 		ft_memcpy(new->content, content, content_size);
-// 		new->content_size = content_size;
-// 	}
-// 	new->next = NULL;
-// 	return (new);
-// }
